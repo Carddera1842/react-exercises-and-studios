609935c9ee9292a1b0b793a6db67519b181069ea
Carddera1842 <carddera1842@gmail.com>
2024-08-22 16:20:02 -0500

    "React Part Two Studio"

--- part2/studio/src/components/Ingredients.jsx
+++ part2/studio/src/components/Ingredients.jsx
@@ -1,5 +1,17 @@
+import recipedata from "./recipe.json";
+
 function IngredientList() {
-   return;
+  let ingredients = recipedata[0].ingredients.map((ingredient, index) => {
+    return <li key={index}>{ingredient}</li>
+  })
+   return(
+    <div>
+      <h3>Ingredients</h3>
+      <ul>
+        {ingredients}
+      </ul>
+    </div>
+   );
  }
  
  export default IngredientList;
